a9bbf7a630634b76b049217630b740f17d07d403
Jagandeep Brar <me@jagandeepbrar.io>
2021-01-04 03:30:10 -0600

    [Express] Remove Lidarr and Radarr routes until integration begins

--- src/server/v1/index.ts
+++ src/server/v1/index.ts
@@ -1,10 +1,6 @@
 import express from 'express';
 import { Sonarr } from '@lunasea-notification-relay/server/v1/sonarr';
-import { Lidarr } from '@lunasea-notification-relay/server/v1/lidarr';
-import { Radarr } from '@lunasea-notification-relay/server/v1/radarr';
 
 // Create a router and use each of the modules' own router instance for each subroute
 export const router = express.Router();
-router.use('/lidarr', Lidarr.instance);
-router.use('/radarr', Radarr.instance);
 router.use('/sonarr', Sonarr.instance);
