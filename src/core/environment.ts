c327504f6873861eb3f0cae7edf5283bb5c3f860
Jagandeep Brar <me@jagandeepbrar.io>
2021-01-20 11:32:01 -0600

    [Environment] Cleanup environment check

--- src/core/environment.ts
+++ src/core/environment.ts
@@ -7,13 +7,17 @@ export namespace Environment {
      * If a value is not found, it will exit the application.
      */
     export const validate = (): void => {
-        if (!process.env.DATABASE_URL) {
-            Logger.fatal('Unable to find DATABASE_URL environment value. Exiting...');
-            process.exit(1);
-        }
-        if (!process.env.RESTRICTED_PACKAGE_NAME) {
-            Logger.fatal('Unable to find RESTRICTED_PACKAGE_NAME environment value. Exiting...');
-            process.exit(1);
-        }
+        if (!process.env.DATABASE_URL) shutdown('DATABASE_URL');
+        if (!process.env.RESTRICTED_PACKAGE_NAME) shutdown('RESTRICTED_PACKAGE_NAME');
+    };
+
+    /**
+     * Shutdown application with a log showing which variable is missing from the environment.
+     *
+     * @param variable Environmental variable to print as missing
+     */
+    const shutdown = (variable: string): void => {
+        Logger.fatal(`Unable to find ${variable} environment value. Exiting...`);
+        process.exit(1);
     };
 }
