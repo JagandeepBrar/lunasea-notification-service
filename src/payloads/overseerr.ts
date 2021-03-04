9f80efbe65ca41a8d9e5a8c01acc1d6e4b08fbf6
Jagandeep Brar <me@jagandeepbrar.io>
2021-03-04 15:49:03 -0600

    [Overseerr] Attach images to payload

--- src/payloads/overseerr.ts
+++ src/payloads/overseerr.ts
@@ -1,5 +1,7 @@
+import { FanartTV } from '../api/fanart';
 import { MediaType, RequestProperties } from '../models/overseerr';
 import { NotificationPayload, payloadTitle } from '../payloads';
+import { Logger } from '../utilities/logger';
 
 const title = (profile: string, body: string): string => payloadTitle('Overseerr', profile, body);
 
@@ -9,9 +11,11 @@ const title = (profile: string, body: string): string => payloadTitle('Overseerr
 export const mediaApprovedPayload = async (data: RequestProperties, profile: string): Promise<NotificationPayload> => {
     const body1 = data.subject;
     const body2 = `Originally Requested by ${data.username ?? 'Unknown User'}`;
+    const image = data.media?.media_type === MediaType.MOVIE ? await getMovieImageURL(data) : await getSeriesImageURL(data);
     return <NotificationPayload>{
         title: title(profile, `${data.media?.media_type === MediaType.MOVIE ? 'Movie' : 'Series'} Approved`),
         body: [body1, body2].join('\n'),
+        image: image,
     };
 };
 
@@ -21,9 +25,11 @@ export const mediaApprovedPayload = async (data: RequestProperties, profile: str
 export const mediaAvailablePayload = async (data: RequestProperties, profile: string): Promise<NotificationPayload> => {
     const body1 = data.subject;
     const body2 = `Originally Requested by ${data.username ?? 'Unknown User'}`;
+    const image = data.media?.media_type === MediaType.MOVIE ? await getMovieImageURL(data) : await getSeriesImageURL(data);
     return <NotificationPayload>{
         title: title(profile, `${data.media?.media_type === MediaType.MOVIE ? 'Movie' : 'Series'} Available`),
         body: [body1, body2].join('\n'),
+        image: image,
     };
 };
 
@@ -33,9 +39,11 @@ export const mediaAvailablePayload = async (data: RequestProperties, profile: st
 export const mediaDeclinedPayload = async (data: RequestProperties, profile: string): Promise<NotificationPayload> => {
     const body1 = data.subject;
     const body2 = `Originally Requested by ${data.username ?? 'Unknown User'}`;
+    const image = data.media?.media_type === MediaType.MOVIE ? await getMovieImageURL(data) : await getSeriesImageURL(data);
     return <NotificationPayload>{
         title: title(profile, `${data.media?.media_type === MediaType.MOVIE ? 'Movie' : 'Series'} Declined`),
         body: [body1, body2].join('\n'),
+        image: image,
     };
 };
 
@@ -45,9 +53,11 @@ export const mediaDeclinedPayload = async (data: RequestProperties, profile: str
 export const mediaFailedPayload = async (data: RequestProperties, profile: string): Promise<NotificationPayload> => {
     const body1 = data.subject;
     const body2 = `Originally Requested by ${data.username ?? 'Unknown User'}`;
+    const image = data.media?.media_type === MediaType.MOVIE ? await getMovieImageURL(data) : await getSeriesImageURL(data);
     return <NotificationPayload>{
         title: title(profile, `${data.media?.media_type === MediaType.MOVIE ? 'Movie' : 'Series'} Failed`),
         body: [body1, body2].join('\n'),
+        image: image,
     };
 };
 
@@ -57,9 +67,11 @@ export const mediaFailedPayload = async (data: RequestProperties, profile: strin
 export const mediaPendingPayload = async (data: RequestProperties, profile: string): Promise<NotificationPayload> => {
     const body1 = data.subject;
     const body2 = `Originally Requested by ${data.username ?? 'Unknown User'}`;
+    const image = data.media?.media_type === MediaType.MOVIE ? await getMovieImageURL(data) : await getSeriesImageURL(data);
     return <NotificationPayload>{
         title: title(profile, `${data.media?.media_type === MediaType.MOVIE ? 'Movie' : 'Series'} Requested`),
         body: [body1, body2].join('\n'),
+        image: image,
     };
 };
 
@@ -71,4 +83,38 @@ export const testPayload = async (data: RequestProperties, profile: string): Pro
         title: title(profile, 'Connection Test'),
         body: 'LunaSea is ready for Overseerr notifications!',
     };
+};
+
+/**
+ * Pull the TVDB ID and fetch the series poster URL
+ *
+ * @param data RequestProperties from Overseerr
+ */
+const getSeriesImageURL = async (data: RequestProperties): Promise<string | undefined> => {
+    try {
+        if (data.media?.tvdbId) {
+            const id = parseInt(data.media.tvdbId);
+            if (!isNaN(id)) return await FanartTV.getSeriesPoster(id);
+        }
+    } catch (error) {
+        Logger.error(error);
+    }
+    return undefined;
+};
+
+/**
+ * Pull the TMDB ID and fetch the movie poster URL
+ *
+ * @param data RequestProperties from Overseerr
+ */
+const getMovieImageURL = async (data: RequestProperties): Promise<string | undefined> => {
+    try {
+        if (data.media?.tmdbId) {
+            const id = parseInt(data.media.tmdbId);
+            if (!isNaN(id)) return await FanartTV.getMoviePoster(id);
+        }
+    } catch (error) {
+        Logger.error(error);
+    }
+    return undefined;
 };
