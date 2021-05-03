f6c4213a74b9a5a6419c15fc70bdada0a7791271
Jagandeep Brar <me@jagandeepbrar.io>
2021-05-02 20:00:32 -0500

    Added custom notification route

--- src/routes/v1.ts
+++ src/routes/v1.ts
@@ -1,14 +1,16 @@
 import express from 'express';
+import { router as Custom } from './v1/custom';
 import { router as Lidarr } from './v1/lidarr';
+import { router as Overseerr } from './v1/overseerr';
 import { router as Radarr } from './v1/radarr';
 import { router as Sonarr } from './v1/sonarr';
 import { router as Tautulli } from './v1/tautulli';
-import { router as Overseerr } from './v1/overseerr';
 
 // Create a router and use each of the modules' own router instance for each subroute
 export const router = express.Router();
+router.use('/custom', Custom);
 router.use('/lidarr', Lidarr);
+router.use('/overseerr', Overseerr);
 router.use('/radarr', Radarr);
 router.use('/sonarr', Sonarr);
 router.use('/tautulli', Tautulli);
-router.use('/overseerr', Overseerr);
---
+++ src/routes/v1/custom.ts
@@ -0,0 +1,32 @@
+import { Server } from '../../server';
+import { Middleware } from '../../server/middleware';
+import { Constants } from '../../utilities/constants';
+import { Logger } from '../../utilities/logger';
+import { handleWebhook } from '../../webhooks/custom';
+import express from 'express';
+
+export const router = express.Router();
+router.post('/user/:id', Middleware.validateUser, Middleware.checkNotificationPassword, Middleware.pullUserTokens, handler);
+router.post('/device/:id', Middleware.extractDeviceToken, handler);
+
+/**
+ * Custom Webhook Handler: Handles a webhook from any source, and sends a notification to all devices that are in `response.locals.tokens`.
+ *
+ * @param request Express request object
+ * @param response Express response object
+ */
+async function handler(request: express.Request, response: express.Response): Promise<void> {
+    Logger.info('Running Custom webhook...');
+    try {
+        Logger.debug('-> Sending HTTP response to complete webhook...');
+        response.status(200).json(<Server.Response>{ message: Constants.MSG_OK });
+        Logger.debug('-> HTTP response sent (200 OK)');
+        await handleWebhook(request.body, response.locals.tokens);
+    } catch (error) {
+        Logger.error(error.message);
+        Logger.debug('-> Sending HTTP response to complete webhook...');
+        response.status(500).json(<Server.Response>{ message: Constants.MSG_INTERNAL_SERVER_ERROR });
+        Logger.debug('HTTP response sent (500 Internal Server Error)');
+    }
+    Logger.info('Finished Custom webhook.');
+}
