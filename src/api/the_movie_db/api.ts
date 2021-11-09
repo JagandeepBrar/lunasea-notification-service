0beafc7f1cb0b2f3ebeedd3977134dedc7f49441
Jagandeep Brar <jagandeep.brar@solarskyrise.com>
2021-11-08 19:23:07 -0500

    release: v1.3.1 (#28)
    * tweak(cache): Cache tmdb image ID only, not entire URL

--- src/api/the_movie_db/api.ts
+++ src/api/the_movie_db/api.ts
@@ -10,17 +10,13 @@ const http = axios.create({
   },
 });
 
-const _constructImageURL = (path: string): string => {
-  return `${Constants.THE_MOVIE_DB.IMAGE.BASE_URL}${Constants.THE_MOVIE_DB.IMAGE.SIZE}${path}`;
-};
-
 export const getMoviePoster = async (movieId: number): Promise<string | undefined> => {
   return http({
     url: `movie/${movieId}`,
   }).then((response): string | undefined => {
     const movie = response.data as ContentResponse;
-    if (movie.poster_path) return _constructImageURL(movie.poster_path);
-    if (movie.backdrop_path) return _constructImageURL(movie.backdrop_path);
+    if (movie.poster_path) return movie.poster_path;
+    if (movie.backdrop_path) return movie.backdrop_path;
     return undefined;
   });
 };
@@ -35,8 +31,8 @@ export const getSeriesPoster = async (seriesId: number): Promise<string | undefi
     const data = response.data as FindContentResponse;
     if (data.tv_results && data.tv_results.length > 0) {
       const series = data.tv_results[0];
-      if (series.poster_path) return _constructImageURL(series.poster_path);
-      if (series.backdrop_path) return _constructImageURL(series.backdrop_path);
+      if (series.poster_path) return series.poster_path;
+      if (series.backdrop_path) return series.backdrop_path;
       return undefined;
     }
   });
